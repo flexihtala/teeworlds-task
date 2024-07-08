a63d60e3496d7b9e51dbc02e35d50070d5658ff2
flexihtala <flexihtala@gmail.com>
2024-07-08 23:52:27 +0500

    Добавил прыжок

--- scripts/entities.py
+++ scripts/entities.py
@@ -7,9 +7,14 @@ class PhysicsEntity:
         self.pos = list(pos)
         self.size = size
         self.velocity = [0, 0]
+        self.collisions = {'up': False, 'down': False,
+                           'right': False, 'left': False}
 
     def update(self, tilemap, movement=(0, 0)):
-        frame_movement = (movement[0] + self.velocity[0], movement[1] + self.velocity[1])
+        self.collisions = {'up': False, 'down': False,
+                           'right': False, 'left': False}
+        frame_movement = (movement[0] + self.velocity[0],
+                          movement[1] + self.velocity[1])
 
         self.pos[0] += frame_movement[0]
         entity_rect = self.rect()
@@ -17,8 +22,10 @@ class PhysicsEntity:
             if entity_rect.colliderect(rect):
                 if frame_movement[0] > 0:
                     entity_rect.right = rect.left
+                    self.collisions['right'] = True
                 if frame_movement[0] < 0:
                     entity_rect.left = rect.right
+                    self.collisions['left'] = True
                 self.pos[0] = entity_rect.x
 
         self.pos[1] += frame_movement[1]
@@ -27,10 +34,14 @@ class PhysicsEntity:
             if entity_rect.colliderect(rect):
                 if frame_movement[1] > 0:
                     entity_rect.bottom = rect.top
+                    self.collisions['down'] = True
                 if frame_movement[1] < 0:
                     entity_rect.top = rect.bottom
+                    self.collisions['up'] = True
                 self.pos[1] = entity_rect.y
         self.velocity[1] = min(5, self.velocity[1] + 0.1)
+        if self.collisions['down'] or self.collisions['up']:
+            self.velocity[1] = 0
 
     def rect(self):
         return pygame.Rect(self.pos[0], self.pos[1],
