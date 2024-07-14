f1756947f5110af0cd282dfb58a898059f32526d
flexihtala <flexihtala@gmail.com>
2024-07-14 14:25:42 +0500

    Мультиплеер (поддерживает только изменение позиции)

--- game.py
+++ game.py
@@ -1,5 +1,8 @@
 import pygame
 import sys
+import socket
+import json
+import threading
 from scripts.settings import *
 from scripts.utils import load_sprite
 from scripts.entities import PhysicsEntity
@@ -34,6 +37,22 @@ class Game:
         self.scroll = [0, 0]
         self.camera_speed = 30
 
+        self.host = '192.168.1.125'
+        self.port = 5555
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.connect((self.host, self.port))
+        self.socket_name = self.client_socket.getsockname()
+        self.address = str(self.socket_name[0]) + ":" + str(
+            self.socket_name[1])
+
+        self.player_info = {}
+        self.players_data = {}
+        self.players = {}
+
+        self.receive_thread = threading.Thread(target=self.receive_data)
+        self.receive_thread.daemon = True
+        self.receive_thread.start()
+
     def run(self):
         while True:
             self.display.fill(BACKGROUND_COLOR)
@@ -47,8 +66,10 @@ class Game:
             render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
 
             self.tilemap.render(self.display, render_scroll)
-            self.player.update(self.tilemap, ((self.movement[1] - self.movement[0]) * 2, 0))
+            self.player.update(self.tilemap,
+                               ((self.movement[1] - self.movement[0]) * 2, 0))
             self.player.render(self.display, render_scroll)
+            self.render_players(render_scroll)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -68,11 +89,40 @@ class Game:
                     if event.key == pygame.K_d:
                         self.movement[1] = False
 
+            self.send_player_info()
             self.screen.blit(
                 pygame.transform.scale(self.display, self.screen.get_size()),
                 (0, 0))
             pygame.display.update()
             self.clock.tick(FPS)
+
+    def send_player_info(self):
+        self.player_info['x'] = self.player.pos[0]
+        self.player_info['y'] = self.player.pos[1]
+        try:
+            self.client_socket.sendall(json.dumps(self.player_info).encode())
+        except Exception as e:
+            print(f"Ошибка при отправлении информации: {e}")
+
+    def receive_data(self):
+        while True:
+            try:
+                data = self.client_socket.recv(1024).decode()
+                if data:
+                    self.players_data = json.loads(data)
+                    self.players_data.pop(self.address)
+                    for addr, pdata in self.players_data.items():
+                        if addr not in self.players:
+                            self.players[addr] = PhysicsEntity(self, (pdata['x'], pdata['y']), (10, 16))
+                        else:
+                            self.players[addr].pos = (pdata['x'], pdata['y'])
+                    print(self.players)
+            except:
+                break
+
+    def render_players(self, render_scroll):
+        for player in self.players.values():
+            player.render(self.display, render_scroll)
 
 
 if __name__ == "__main__":
---
+++ server.py
@@ -0,0 +1,50 @@
+import socket
+import threading
+import json
+
+
+class GameServer:
+    def __init__(self):
+        self.host = '192.168.1.125'
+        self.port = 5555
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.clients = []
+        self.players = {}
+        self.server_socket.bind((self.host, self.port))
+        self.server_socket.listen()
+        print('Сервер запущен, ожидание подключения')
+
+    def handle_client(self, conn, addr):
+        try:
+            while True:
+                data = conn.recv(1024).decode()
+                if not data:
+                    break
+                player_data = json.loads(data)
+                self.players[addr] = player_data
+                print(self.players)
+                for client in self.clients:
+                    client.sendall(json.dumps(self.players).encode('utf-8'))
+        except Exception as e:
+            print(e)
+        finally:
+            conn.close()
+            self.clients.remove(conn)
+            self.players.pop(addr)
+            print(f'Соединение с {addr} разорвано')
+
+    def start(self):
+        try:
+            while True:
+                conn, addr = self.server_socket.accept()
+                address = str(addr[0]) + ":" + str(addr[1])
+                print(f'Подключен к адресу {addr}')
+                self.clients.append(conn)
+                threading.Thread(target=self.handle_client,
+                                 args=(conn, address)).start()
+        except Exception as e:
+            print(e)
+
+
+if __name__ == "__main__":
+    GameServer().start()
